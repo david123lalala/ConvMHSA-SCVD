19bdf94d015cf73b193b44134d62164e9b018052
david123lalala <13065742851@163.com>
2023-07-15 21:01:56 +0800

    Update vectorize_fragment.py

--- vectorize_fragment.py
+++ vectorize_fragment.py
@@ -13,7 +13,6 @@ target_line4='for'
 target_line4='while'
 
 
-# Sets for operators
 operators3 = {'<<=', '>>='}
 operators2 = {
     '->', '++', '--',
@@ -29,48 +28,25 @@ operators1 = {
     '{', '}'
 }
 
-"""
-Functionality to train Word2Vec model and vectorize fragments
-Trains Word2Vec model using list of tokenized fragments
-Uses trained model embeddings to create 2D fragment vectors
-"""
-
 
 class FragmentVectorizer:
     def __init__(self, vector_length):
         self.fragments = []
         self.vector_length = vector_length
-        #forward_slices的意思
-        #backward——slices的意思
         self.forward_slices = 0
         self.backward_slices = 0
 
-    """
-    Takes a line of solidity code (string) as input
-    Tokenizes solidity code (breaks down into identifier, variables, keywords, operators)
-    Returns a list of tokens, preserving order in which they appear
-    """
-    #输入solidity的一行代码
-    #tmp用来保存solidity的一段序列，w保存一个完整的单词
     @staticmethod
     def tokenize(line):
         tmp, w = [], []
         i = 0
-        
-        # if re.search(target_line2,line,flags=0):
-            # tmp=[]
-        # elif re.search(target_line3,line,flags=0):
-            # tmp=[]
-        # else:
+
         while i < len(line):
-            # Ignore spaces and combine previously collected chars to form words
             if line[i] == ' ':
-                    #''.join表示生成新的字符串
                 tmp.append(''.join(w))
                 tmp.append(line[i])
                 w = []
                 i += 1
-                    # Check operators and append to final list
             elif line[i:i + 3] in operators3:
                 tmp.append(''.join(w))
                 tmp.append(line[i:i + 3])
@@ -86,32 +62,19 @@ class FragmentVectorizer:
                 tmp.append(line[i])
                 w = []
                 i += 1
-                # Character appended to word list
             else:
                 w.append(line[i])
                 i += 1
-                # Filter out irrelevant strings
 
         res = list(filter(lambda c: c != '', tmp))
-        # res = list(filter(lambda c: c != 'call', res))
-        # res = list(filter(lambda c: c != 'value', res))
         return list(filter(lambda c: c != ' ', res))
-
-
-    """
-    Tokenize entire fragment
-    Tokenize each line and concatenate to one long list
-    """
-
-    #将fragment转变成tokenize
+    
     @staticmethod
     def tokenize_fragment(fragment):
         tokenized = []
-        #re.compile 正则表达式生成对象
         function_regex = re.compile('function(\d)+')
         backwards_slice = False
         for line in fragment:
-            #向量化line
             tokens = FragmentVectorizer.tokenize(line)
             tokenized += tokens
             if len(list(filter(function_regex.match, tokens))) > 0:
@@ -119,11 +82,6 @@ class FragmentVectorizer:
             else:
                 backwards_slice = False
         return tokenized, backwards_slice
-
-    """
-    Add input fragment to model
-    Tokenize fragment and buffer it to list
-    """
 
     def add_fragment(self, fragment):
         tokenized_fragment, backwards_slice = FragmentVectorizer.tokenize_fragment(fragment)
@@ -133,13 +91,6 @@ class FragmentVectorizer:
         else:
             self.forward_slices += 1
 
-    """
-    Uses Word2Vec to create a vector for each fragment
-    Gets a vector for the fragment by combining token embeddings
-    Number of tokens used is min of number_of_tokens and 100
-    """
-
-    #word2vector将每个fragment转为词向量，最后返回一个fragment的词向量
     def vectorize(self, fragment):
         tokenized_fragment, backwards_slice = FragmentVectorizer.tokenize_fragment(fragment)
         vectors = np.zeros(shape=(100, self.vector_length))
@@ -155,10 +106,7 @@ class FragmentVectorizer:
     Done adding fragments, then train Word2Vec model
     Only keep list of embeddings, delete model and list of fragments
     """
-    
-    #用word2vector来训练词向量
     def train_model(self):
-        # Set min_count to 1 to prevent out-of-vocabulary errors
         model = Word2Vec(self.fragments, min_count=1, vector_size=self.vector_length, sg=0)  # sg=0: CBOW; sg=1: Skip-Gram
         self.embeddings = model.wv
         del model
